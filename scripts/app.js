577e0daa2f1f22f93090b865e56a9a73e2af08f4
aapiane09 <aapiane09@gmail.com>
2016-12-02 18:38:05 -0800

    Did jQuery Dom Lab

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,15 @@ $( document ).ready(function whenLoaded(){
 
   // Your jQuery here
   // 1. Get the value "Hello There".
-
+$('#greeting').text();
   // 2. Change (or set) the value to "Hola".
-
+$('#greeting').text('Hola');
   // 3. Add an h1 to the page that says "A Spanish Greeting".
-
+var $newHOne = $('<h1>A Spanish Greeting</h1>');
+$('#greeting').prepend($newHOne);
   // 4. Add a second greeting ("Que tal?"), in a second column to the right of the first.
-
+var $newBootstrapDiv = $('<div class=col-sm-6>Que Tal?</div>');
+$('.row').append($newBootstrapDiv);
   // 5. Add the class "blue" to both greetings.
-
+$('.col-sm-6').addClass("blue");
 });
